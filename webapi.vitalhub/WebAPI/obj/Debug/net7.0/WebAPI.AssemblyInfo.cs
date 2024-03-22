ed01ed086d7481e87df15845a2df00dfb92c3e0f
Rebeca <rebecacarolina75@gmail.com>
2024-03-22 15:01:16 -0300

    .

--- webapi.vitalhub/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ webapi.vitalhub/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8fb0bcc08f31ee8640603cf6ed1d0a688464dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5170af12867cb4743c0d9894897a5126bfed906b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
